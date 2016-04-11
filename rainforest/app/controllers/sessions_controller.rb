fd5f5879de5d1e0f2e73097383b28a01bf62d720
Kimberly Oleiro <koleiro@gmail.com>
2016-04-11 18:51:58 -0400

    Creating log in functionality

---
+++ rainforest/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+#we don't have to instantiate a user object because we are only logging in, not creating new user
+
+  def create
+    user = User.find_by(email: params[:email]) #find email by email user types in
+    if user && user.authenticate(params[:password]) #check if user exists & authenticated with password they type in
+      session[:user_id] = user.id     #session hash key value pair, # session key has been assigned
+      redirect_to products_url
+    else
+      render "new"
+    end
+  end
+
+#logging in/setting hash in browser, lets server reference as user navigates through application.
+  def destroy
+    session[:user_id] = nil
+    redirect_to products_url, notice: "Logged out!"
+  end
+end
--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-
   root 'products#index'
 
   resources :products
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
 
  #  Prefix Verb   URI Pattern                  Controller#Action
